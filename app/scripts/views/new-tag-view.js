a33e78d698026c0db3995b63d0fc2182d74f3b3f
Niklas Andreasson <eaglus_@hotmail.com>
2013-08-13 22:03:11 +0200

    Reindent on html etc

--- app/scripts/views/new-tag-view.js
+++ app/scripts/views/new-tag-view.js
@@ -1,44 +1,52 @@
 /*global define*/
 
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'templates'
-], function ($, _, Backbone, JST) {
+  'jquery',
+  'underscore',
+  'backbone',
+  'templates'
+], function ($, _, Backbone) {
     'use strict';
 
-  var NewTagView = Backbone.View.extend({
-    el: '#new-tag',
+    var NewTagView = Backbone.View.extend({
+      el: '#new-tag',
 
-    events: {
-      'click #new-tag-button': 'createNewTag'
-    },
+      events: {
+        'click #new-tag-button': 'createNewTag'
+      },
 
-    initialize: function() {
-      this.$nameInput = this.$('#name');
-      this.$latInput  = this.$('#lat');
-      this.$longInput = this.$('#long');
-      this.$descInput = this.$('#desc');
-    },
+      initialize: function() {
+        this.$nameInput = this.$('#name');
+        this.$latInput  = this.$('#lat');
+        this.$lngInput = this.$('#lng');
+        this.$descInput = this.$('#desc');
+      },
 
-    createNewTag: function(e) {
-      e.preventDefault();
+      newAttributes: function() {
+        return {
+          name: this.$nameInput.val(),
+          lat: +this.$latInput.val(),
+          lng: +this.$lngInput.val(),
+          desc: this.$descInput.val()
+        };
+      },
 
-      var newAttributes = {
-        name: this.$nameInput.val(),
-        lat: +this.$latInput.val(),
-        lng: +this.$longInput.val(),
-        desc: this.$descInput.val()
-      };
+      clearAttributes: function() {
+        this.$nameInput.val('');
+        this.$latInput.val('');
+        this.$lngInput.val('');
+        this.$descInput.val('');
+      },
 
-      this.collection.create( newAttributes );
-    },
+      createNewTag: function( e ) {
+        e.preventDefault();
 
-    render: function() {
+        this.collection.create( this.newAttributes() );
+        this.clearAttributes();
+      },
 
-    }
+      render: function() { }
+    });
+
+    return NewTagView;
   });
-
-  return NewTagView;
-});
